1adc9e7fffc93a248a517ebe450e69acff49eb14
ubtu_pengqiao <740995710@qq.com>
2023-08-11 14:49:54 +0800

    修改sort为快速排序

--- main.cpp
+++ main.cpp
@@ -2,33 +2,70 @@
 
 using namespace std;
 
-
+#if 0
 void sort(int arr[],int size)
 {
-	int tmp = 0;
-	for(int i =0;i<size-1;++i)
-	{
-		for(int j=0;j<size-1-i;++j)
-		{
-			if(arr[j] >arr[j+1])
-			{
-				tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-			}
-		}
-	}|
+	// int tmp = 0;
+	// for(int i =0;i<size-1;++i)
+	// {
+	// 	for(int j=0;j<size-1-i;++j)
+	// 	{
+	// 		if(arr[j] >arr[j+1])
+	// 		{
+	// 			tmp = arr[j];
+	// 			arr[j] = arr[j+1];
+	// 			arr[j+1] = tmp;
+	// 		}
+	// 	}
+	// }|
 
 }
 
+#endif
+
+
+void sort(int arr[],int i,int j)
+{
+	int val = arr[i];
+
+	int l = i;
+	int r = j;
+
+	while (l < r)
+	{
+		while (l< r && arr[r] >= val)
+		{
+			r--;
+		}
+		if(l < r)
+		{
+			arr[l++] = arr[r];
+		}
+
+		while (l< r && arr[l] <= val)
+		{
+			l++;
+		}
+		if(l < r)
+		{
+			arr[r--] = arr[l];
+		}
+
+	}
+	all[l] = val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+}
 int main()
 {	
 	int arr[] = {12,4,89,43,21,78};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
-	printf("windows");
-	//
-
-	cout<<"linux"<<endl;
+	
+	for(int i =0;i <size ; ++i)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 	return 0;
 }
